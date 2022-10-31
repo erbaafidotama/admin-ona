1ab1eec44d8468ad620f0d21d2d13e72f7645c39
erbaafidotama <erbaafidotama13@gmail.com>
2022-10-31 17:10:11 +0700

    example navigate to professional info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+
 
 function Home() {
+  const navigate = useNavigate();
+
+  const gotoProfessionalInfoForm = () => {
+    navigate("/professionalInfo")
+  }
   return (
     <div>
       <h1>This is home page.</h1>
+      <h2 className="text-2xl cursor-pointer" onClick={gotoProfessionalInfoForm}>Click This: Professional Info Form</h2>
     </div>
   )
 }
